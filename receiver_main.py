dac5cd02530cc5ecac1a99b6ba93542bab04fd91
hsidd1 <hamzasiddiqui247@gmail.com>
2023-07-24 10:10:22 -0400

    receive manual runs (radar only)

--- receiver_main.py
+++ receiver_main.py
@@ -2,7 +2,7 @@ import paho.mqtt.client as mqtt
 import time
 import yaml
 import subprocess
-
+from processModule.serverConnect import connect_mqtt
 """
 PC client as subscriber of both device clients for logging received published data.
 """
@@ -13,19 +13,27 @@ def on_message(client, userdata, message):
 with open("config.yaml", "r") as f:
     config = yaml.safe_load(f)
 
-client = mqtt.Client("PC")
-client.connect(config["mqtt"]["broker"])
+#client = mqtt.Client("PC")
+#client.connect(config["mqtt"]["broker"])
+
+def subscribe(client, topic):
+    def on_message(client, userdata, msg):
+        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+
+    client.subscribe(topic)
+    client.on_message = on_message
 
 def main():
-    client.on_message = on_message
-    client.loop_start()
-    subprocess.call(["python", "radar_client.py"])
-    subprocess.call(["python", "cam_client.py"])
+    # subprocess.run(["python", "radar_client.py"])
+    # subprocess.run(["python", "cam_client.py"])
     #client.subscribe("data") # subscribes to both radar and cam
-    client.subscribe("data/radar")
-    client.subscribe("data/camera")
-    time.sleep(1)
-    client.loop_stop()
+
+    client = connect_mqtt("PC")
+    subscribe(client, topic = "data/radar")
+    subscribe(client, topic = "data/camera/frame")
+    subscribe(client, topic = "data/camera/ts")
+    client.loop_forever()
+    #time.sleep(1)
 
 if __name__ == "__main__":
     main()
