89e8630beb9d938fc48ca67fbeb0537d8fd3c713
hsidd1 <hamzasiddiqui247@gmail.com>
2023-07-24 10:11:23 -0400

    alt server, reformat publish (todo)

--- cam_client.py
+++ cam_client.py
@@ -2,12 +2,15 @@ import paho.mqtt.client as mqtt
 import cv2
 import yaml
 import datetime as dt
+import time 
+from processModule.serverConnect import connect_mqtt
 
 with open("config.yaml", "r") as f:
     config = yaml.safe_load(f)
 
-client = mqtt.Client(config["mqtt"]["client_id2"]) # camera
-client.connect(config["mqtt"]["broker"]) # web server 
+#client = mqtt.Client(config["mqtt"]["client_id2"]) # camera
+#client.connect(config["mqtt"]["broker"]) 
+client = connect_mqtt(config["mqtt"]["client_id2"]) # emqx web server
 # client.subscribe("data/radar") 
 
 def on_log(client, userdata, level, buf):
@@ -24,7 +27,7 @@ if config["mqtt"]["show_log"]:
 
 cap = cv2.VideoCapture(config["Files"]["video_file"])
 
-client.loop_start()
+# client.loop_start()
 while True:
     ret, frame = cap.read()
     if not ret:
@@ -33,6 +36,7 @@ while True:
     client.publish(topic="data/camera/frame", payload=msg, qos=0) # QoS 0 for frames
     timestamp = dt.datetime.now().isoformat()
     client.publish(topic="data/camera/ts", payload=timestamp, qos=1) # QoS 1 for timestamps
-client.loop_stop()
+    time.sleep(1)
+# client.loop_stop()
 cap.release()
 cv2.destroyAllWindows()
