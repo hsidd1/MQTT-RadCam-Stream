c7508cbe4eff2948514245cc37100b614d660da5
hsidd1 <hamzasiddiqui247@gmail.com>
2023-07-24 10:13:33 -0400

    publish for rad data (passed)

--- radar_client.py
+++ radar_client.py
@@ -2,12 +2,15 @@ import paho.mqtt.client as mqtt
 import yaml
 from processModule.rd_process import data
 import time
+from processModule.serverConnect import connect_mqtt
 
 with open("config.yaml", "r") as f:
     config = yaml.safe_load(f)
 
-client = mqtt.Client(config["mqtt"]["client_id1"]) # radar
-client.connect(config["mqtt"]["broker"])
+#client = mqtt.Client(config["mqtt"]["client_id1"]) # radar
+#client.connect(config["mqtt"]["broker"])
+client = connect_mqtt(config["mqtt"]["client_id1"]) # emqx web server
+
 # client.subscribe("data/camera") # receive frame data from cam topic
 
 def on_log(client, userdata, level, buf):
@@ -27,5 +30,5 @@ while data:
         msg = data[i:i+10]  # modify to publish based on timestamps for intervals
         data = data[i+10:]
         client.publish(topic="data/radar", payload=str(msg), qos=0)
-    time.sleep(4)
+        time.sleep(3)
 client.loop_stop()
