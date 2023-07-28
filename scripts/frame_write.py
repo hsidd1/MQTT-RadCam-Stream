3f0285a1b6929ed7726924b80804c81ffac0a173
hsidd1 <hamzasiddiqui247@gmail.com>
2023-07-28 05:16:57 -0400

    testing test workflow

---
+++ scripts/frame_write.py
@@ -0,0 +1,20 @@
+import cv2
+import yaml
+
+"""
+Script to write a sample frame to file for testing purposes.
+Bytearray of the frame is written to data/sample_frame.txt since this
+object is extremely large
+"""
+
+with open("config.yaml", "r") as f:
+    config = yaml.safe_load(f)
+
+if __name__ == '__main__':
+    cap = cv2.VideoCapture(config["Files"]["video_file"])
+    ret, frame = cap.read()
+    with open("data/sample_frame.txt", "a") as f:
+        f.write(str(bytearray(frame)))
+    print("Sample frame written to data/sample_frame.txt")
+    cap.release()
+    cv2.destroyAllWindows()
