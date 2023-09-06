d8c2820b6f22c9614c658ec8196b0eabb1f328f1
hsidd1 <hamzasiddiqui247@gmail.com>
2023-09-05 23:55:45 -0400

    fps timing template to test on other devices

--- live_cameraclient.py
+++ live_cameraclient.py
@@ -29,13 +29,17 @@ def publish(client):
         client.on_message = on_message
 
     global cap
-
-    cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)  # external camera
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)  # external camera
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, config["LiveData"]["camera"]["width"])
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, config["LiveData"]["camera"]["height"])
     cap.set(cv2.CAP_PROP_FPS, config["LiveData"]["camera"]["fps"])
     client = connect_mqtt("Camera")
+    # num_frames = 120
+    # start = time.perf_counter()
+    # i = 0
     while True:
+        # if i == num_frames:
+        #    break
         try:
             ret, frame = cap.read()
             if ret:
@@ -45,18 +49,20 @@ def publish(client):
                 payload = bytearray(frame)
                 # extend payload with timestamp: 26 bytes
                 payload.extend(bytearray(str(dt.datetime.now().isoformat()), "utf-8"))
+
                 # print(f"Timestamp: {str(dt.datetime.now().isoformat())}")
                 # print(len(bytearray(str(dt.datetime.now().isoformat()), "utf-8"))) # 26 bytes
                 res = client.publish("data/livecamera", payload=payload, qos=0)
                 status = res[0]
                 if status == 0:
                     print(f"Send {len(payload)} bytes to topic data/livecamera")
+                    #i += 1
             else:
                 print("No frame received.")
         except KeyboardInterrupt:
             print("LIVE CAMERA: Process Terminated. Exiting Camera...")
             break
-
+    #print(f"FPS is {num_frames/(time.perf_counter()-start)}")
 
 def main():
     try:
