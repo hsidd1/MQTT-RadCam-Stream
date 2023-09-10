c85b5ba3456d3a3aad3776028f8378cbb7d1b32e
hsidd1 <hamzasiddiqui247@gmail.com>
2023-09-10 02:45:16 -0400

    bring back clearing scripts

---
+++ scripts/clear_logs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+A script to easily delete all logs quickly instead of manually deleting them.
+"""
+import os
+
+RADCAMLOG = "../liveDataLog/radcam_log.json"
+IMGS = "../liveDataLog/camera_data/"
+
+with open (RADCAMLOG, "w") as f:
+    f.write("")
+    print("Cleared radcam_log.json")
+
+# delete all jpgs
+for file in os.listdir(IMGS):
+    if file.endswith(".jpg"):
+        os.remove(IMGS + file)
+        #print(f"Deleted {file}")
+print("Cleared camera_data folder.")
